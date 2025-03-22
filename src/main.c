3fef08e553fe1f60f31045f62e061185dc2413dc
Spyer123 <kurber.ru@yandex.ru>
2025-03-22 10:53:09 +0300

    added variables

--- src/main.c
+++ src/main.c
@@ -12,17 +12,36 @@
 #include <ch32l103.h>
 #endif
 #include <debug.h>
+#include <time.h>
 
 
-#define BLINKY_GPIO_PORT GPIOD
-#define BLINKY_GPIO_PIN_1 GPIO_Pin_3
-#define BLINKY_GPIO_PIN_2 GPIO_Pin_2
 #if defined(CH32L10X)
 #define BLINKY_CLOCK_ENABLE RCC_PB2PeriphClockCmd(RCC_PB2Periph_GPIOC, ENABLE)
 #else
 #define BLINKY_CLOCK_ENABLE RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE)
 #endif
 
+
+// indication:
+#define LINK_LED_PORT GPIOD
+#define LINK_LED_PIN GPIO_Pin_2
+
+
+// battery:
+#define BATT_ADC_CHANNEL 7
+#define BATT_CTL_PORT GPIOD
+#define BATT_CTL_PIN GPIO_Pin_3
+#define BATT_TIMES_OF_MEASURINGS 5
+
+
+// light:
+#define LIGHT_ADC_CHANNEL 6
+#define LIGHT_CTL_PORT GPIOD
+#define LIGHT_CTL_PIN GPIO_Pin_5
+#define LIGHT_TIMES_OF_MEASURINGS 5
+
+#define MAIN_LED_PORT GPIOD
+#define MAIN_LED_PIN GPIO_Pin_0
 
 
 
@@ -36,6 +55,8 @@ void tauno_ADC_init(u8 channel)
 
     switch (channel) {
         case 0:  // PA2
+            RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+            break;
         case 1:  // PA1
             RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
             break;
@@ -43,12 +64,19 @@ void tauno_ADC_init(u8 channel)
             RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
             break;
         case 3:  // PD2
+            RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
+            break;
         case 4:  // PD3
+            RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
+            break;
         case 5:  // PD5
             RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
+            break;
         case 6:  // PD6
             RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
+            break;
         case 7:  // PD4
+            RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
             break;
         default:
             break;
@@ -99,10 +127,9 @@ void tauno_ADC_init(u8 channel)
         case 3:  // PD2
         case 4:  // PD3
         case 5:  // PD5
-            GPIO_Init(GPIOD, &GPIO_InitStructure);
         case 6:  // PD6
-            GPIO_Init(GPIOD, &GPIO_InitStructure);
         case 7:  // PD4
+            GPIO_Init(GPIOD, &GPIO_InitStructure);
             break;
         default:
             break;
@@ -161,63 +188,88 @@ u16 Get_ADC_Average(u8 times, u8 channel)
 }
 
 
-float getVoltage()
+uint16_t getVoltage()
 {
+    uint16_t ADCbits;
 
-    float rescoef = 1.5f;
-    float vref = 3.3f;
-    u16 ADCbits;
+    GPIO_WriteBit(BATT_CTL_PORT, BATT_CTL_PIN, 1);
+    Delay_Ms(10);
+    ADCbits = Get_ADC_Average(BATT_TIMES_OF_MEASURINGS, BATT_ADC_CHANNEL);
+    GPIO_WriteBit(BATT_CTL_PORT, BATT_CTL_PIN, 0);
+    Delay_Ms(10);
 
-    GPIO_WriteBit(GPIOA, GPIO_Pin_1, 1);
-    ADCbits = Get_ADC_Average(5, 6);
-    GPIO_WriteBit(GPIOA, GPIO_Pin_1, 0);
-
-
-    float adcVoltage = (vref * ADCbits * rescoef)/1024.0f;
+    return ADCbits;
+}
 
 
-    return adcVoltage;
+void pinsInitialization(){
+
+
+    GPIO_InitTypeDef GPIO_InitStructure = {0};
+
+
+    GPIO_InitStructure.GPIO_Pin = BATT_CTL_PIN;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(BATT_CTL_PORT, &GPIO_InitStructure);
+
+    GPIO_InitStructure.GPIO_Pin = LIGHT_CTL_PIN;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(LIGHT_CTL_PORT, &GPIO_InitStructure);
+
+    GPIO_InitStructure.GPIO_Pin = MAIN_LED_PIN;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(MAIN_LED_PORT, &GPIO_InitStructure);
+
+    GPIO_InitStructure.GPIO_Pin = LINK_LED_PIN;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(LINK_LED_PORT, &GPIO_InitStructure);
+
 }
 
 
 int main(void) {
-    uint16_t adc_val;  // 0-1023
-    u16 i;
 
     Delay_Init();
     USART_Printf_Init(115200);
-    Delay_Ms(3000);  // Give serial monitor time to open
+    Delay_Ms(500);
 
-//    tauno_ADC_init(6);
-    tauno_ADC_init(5);
+    tauno_ADC_init(BATT_ADC_CHANNEL);
+    tauno_ADC_init(LIGHT_ADC_CHANNEL);
+
+
 
     ADC_RegularChannelConfig(ADC1, ADC_Channel_2, 1, ADC_SampleTime_241Cycles);
     ADC_SoftwareStartConvCmd(ADC1, ENABLE);
     Delay_Ms(50);
     ADC_SoftwareStartConvCmd(ADC1, DISABLE);
 
-    //        printf("%d.%04d\n", wholePart, decimalPart);
 
-    GPIO_InitTypeDef GPIO_InitStructure = {0};
 
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
+    pinsInitialization();
 
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOD, &GPIO_InitStructure);
 
-    GPIO_WriteBit(GPIOD, GPIO_Pin_4, 1);
+
+
+
+    GPIO_WriteBit(MAIN_LED_PORT, MAIN_LED_PIN, 1);
 
     while (1) {
 
-        int LED_input = Get_ADC_Average(5,5);
+        GPIO_WriteBit(LIGHT_CTL_PORT, LIGHT_CTL_PIN, 1);
+        Delay_Ms(10);
+        int LED_input = Get_ADC_Average(LIGHT_TIMES_OF_MEASURINGS,LIGHT_ADC_CHANNEL);
+        GPIO_WriteBit(LIGHT_CTL_PORT, LIGHT_CTL_PIN, 0);
+        Delay_Ms(10);
 
-        printf("%08d\n", LED_input);
-//        printf("%d\n", LED_input == 0);
+        time_t seconds;
+
+        seconds = time(NULL);
+
+        printf("%04d\n", LED_input);
 
 
     }
